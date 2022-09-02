3bccff582af72e3c555029f0994e81f408bed443
seonghun <eptkdltm12@gmail.com>
2022-09-02 11:51:52 +0900

    feat: token exception

--- backend/src/main/java/com/stackoverflow/backend/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/stackoverflow/backend/exception/GlobalExceptionHandler.java
@@ -6,15 +6,18 @@ import org.hibernate.hql.internal.ast.ErrorReporter;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
+
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.validation.FieldError;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestControllerAdvice
