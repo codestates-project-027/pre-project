4cb7211baf2e05dcde41c7c1cc01f55f67b5deeb
random-olive <104327907+random-olive@users.noreply.github.com>
2022-09-06 00:09:45 +0900

    fix:

--- frontend/client/src/Pages/LandingPage.js
+++ frontend/client/src/Pages/LandingPage.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-// import img from '../assets/landingimg.png';
 import img from '../assets/landingScroll.png';
 
 const LandingPage = () => {
@@ -11,7 +10,6 @@ const LandingPage = () => {
   return (
     <div className="landingpage-container">
       <Div>
-        {/* <img className="landing_img" alt="landingimg" src={img} /> */}
         <img className="landing_img" alt="landing img" src={img} />
       </Div>
     </div>
